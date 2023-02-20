cd4ef79b519253bbf9f63d2c79f647bfd5f5fb5b
SQLAdmin <abuyeahia24@gmail.com>
2023-02-20 21:58:11 +0600

    again added with recap 5 things

--- js/bals.js
+++ js/bals.js
@@ -5,3 +5,24 @@ function calculateSalary(salary, tax=0.25, bonus=0){
     return total;
 }
 // template string
+const elementHTML = `
+<div>
+
+<h3> Name: </h3>
+<p>Address: </p>
+<p>Salary: ${calculateSalary(10000, 0, 0)} </p>
+<p>others: ${Number[2]} </p>
+
+</div>
+`
+// arrow function
+const doubleIt = x => x * 2;
+const calculateSalary2 = (salary, tax, bonus) => salary - salary * tax + bonus;
+
+// spread
+const ages = [11, 13, 15, 14, 10, 16];
+const newAges = [...ages, 22, 24, 23];
+
+// destructuring 
+const {x, y} = {x:45, y:12, z:33, namme:'sakib al hasan', salary:45000};
+const [a, b, ...r] = [12, 45, 21, 65, 98];
