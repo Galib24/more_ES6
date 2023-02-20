03c08ac40b713df56016ef4bf857ef540eda431f
SQLAdmin <abuyeahia24@gmail.com>
2023-02-21 01:51:29 +0600

    foreach and map same added

---
+++ js/foreach.js
@@ -0,0 +1,11 @@
+const firends = ['Tom Hanks','Tom Cruise', 'Tom Brady', 'Tom Solaiman'];
+firends.forEach(friend => console.log(friend));
+
+const products = [
+    {id:1, name: 'laptop', price: 45000},
+    {id:1, name: 'Mobile', price: 85000},
+    {id:1, name: 'Tablet', price: 35000},
+    {id:1, name: 'Watch', price: 23000},
+];
+
+products.forEach(product => console.log(product));
