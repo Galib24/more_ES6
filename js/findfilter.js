cb1dbcd978d203301d6901d9bdf53de90e9f7f4a
SQLAdmin <abuyeahia24@gmail.com>
2023-02-22 00:14:07 +0600

    again added

--- js/findfilter.js
+++ js/findfilter.js
@@ -2,5 +2,5 @@ const numbers = [12,32,25,75,13,54,97,25];
 const names = ['abul', 'babul', 'kabul','dadul']
 
 // const x = numbers.find((n) => n >= 50)
-const x = numbers.find((n) => n >= 50)
+const x = numbers.filter((n) => n >= 50)
 console.log(x);
