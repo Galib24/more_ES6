673f41011d68ec040fef289824ca777832b669bc
SQLAdmin <abuyeahia24@gmail.com>
2023-02-21 22:15:58 +0600

    added ternery and templt or js

---
+++ js/arrayspread.js
@@ -0,0 +1,20 @@
+const number = [2,4,1,9,3,5,26,7];
+const number2 = [32,45,67];
+
+// console.log(Math.min(...number))
+// console.log(...number)
+
+// const newArr = [123,21,24,32,...number2,...number]
+// console.log(newArr);
+
+const arr1 = [1,2,3,4,5];
+
+const arr2 = [...arr1];
+
+arr1.push(99)
+arr2.push(55)
+
+// console.log(arr1);
+// console.log(arr2);
+
+const arr = [1,2,3,4,5,6,7];
