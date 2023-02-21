a477f68961c406b67ef66450d1a7493ce6154277
SQLAdmin <abuyeahia24@gmail.com>
2023-02-21 16:21:45 +0600

    practice solve 1 and 2

---
+++ js/practice.js
@@ -0,0 +1,10 @@
+// const array = [1, 3, 5, 7, 9]
+
+// first problem
+const even = [1, 3, 5, 7, 9].map(a => a+1);
+console.log(even);
+
+// second 
+const numbers = [33,50,79,78,90,101,30];
+const divideAble = numbers.filter(num => num % 10 === 0);
+console.log(divideAble);
