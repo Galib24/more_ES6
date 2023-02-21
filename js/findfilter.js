9ac0e3bd7fc2921ea5a812cabc91cba7d518a846
SQLAdmin <abuyeahia24@gmail.com>
2023-02-22 00:51:34 +0600

    added find operation

--- js/findfilter.js
+++ js/findfilter.js
@@ -3,4 +3,19 @@ const names = ['abul', 'babul', 'kabul','dadul']
 
 // const x = numbers.find((n) => n >= 50)
 const x = numbers.filter((n) => n >= 50)
-console.log(x);
+// console.log(x);
+
+const student = [
+    {name:'babull', email: 'babul@gmail.com', avg: 32, fiftyPercent:true},
+    {name:'kabull', email: 'babul@gmail.com', avg: 32, fiftyPercent:true},
+    {name:'mabull', email: 'babul@gmail.com', avg: 32, fiftyPercent:true},
+    {name:'labull', email: 'babul@gmail.com', avg: 32, fiftyPercent:true},
+    {name:'wabull', email: 'babul@gmail.com', avg: 22, fiftyPercent:true},
+    {name:'aabull', email: 'babul@gmail.com', avg: 42, fiftyPercent:true},
+    {name:'cabull', email: 'babul@gmail.com', avg: 42, fiftyPercent:true},
+    {name:'qabull', email: 'babul@gmail.com', avg: 57, fiftyPercent:true}
+]
+
+// const scic = student.filter(s => s.avg >= 50 && s.fiftyPercent === true)
+// const names = scic.map((st) => st.name)
+// console.log(names);
