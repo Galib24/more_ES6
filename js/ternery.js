aafc55c70b0056870332c54961ebbd2b4da97a8a
SQLAdmin <abuyeahia24@gmail.com>
2023-02-21 22:16:15 +0600

    ternery and templet added js

---
+++ js/ternery.js
@@ -0,0 +1,11 @@
+const isStudent = false;
+let x = 5;
+let paragraph = '';
+// if(isStudent === true){
+//     para = 'he is student'
+// }else{
+//     para = 'dont want study'
+// }
+// (x === 5) ? console.log('he is student') : console.log('not student')
+paragraph = (isStudent === true) ? 'he is student' : 'he is not student'
+console.log(paragraph)
---
+++ js/tmplet.js
@@ -0,0 +1,10 @@
+const Name = 'galib';
+const age = 43;
+const job = 'bekar';
+
+// const sentence = 'amar nam ' + Name + 'amar boyos ' + age + 'ami '+job;
+const sentence = `Amar Naam ${Name}
+amar boyos ${age}
+ami ${job}
+`
+console.log(sentence)
